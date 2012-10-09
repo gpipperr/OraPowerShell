247779271c295513f55c37ca1b2c118652952d37
gpipperr <gpipperr@jupiter.fritz.box>
2012-10-09 13:32:32 +0200

    Sign and unsign scripts Handling can be done with the helper script certScripts.ps1

--- certScripts.ps1
+++ certScripts.ps1
@@ -52,10 +52,10 @@ function local-createPrivCert {
 	# check for cert programm
 	local-print  -Text "Info --  Check for the cert programm"
 
-	
-	if ($pfxPass -eq $null) {
-		$pfxPass = read-host "Enter the certificate password" -assecurestring
-	}    
+	# password must be entered in window!
+	#if ($pfxPass -eq $null) {
+	#	$pfxPass = read-host "Enter the certificate password" -assecurestring
+	#}    
 	
 	# try to find the cert programm
 	# 
@@ -114,17 +114,17 @@ function local-createPrivCert {
 	
 		# create a local root certificate
 		local-print  -Text "Info -- create a local root certificate"
-		& $cert_prog -n "CN=Local Certificate" -a sha1 -eku 1.3.6.1.5.5.7.3.3 -r -sv root.pvk root.cer -ss Root -sr localMachine
+		& $cert_prog -n "CN=Local Selfsign Certificate" -a sha1 -eku 1.3.6.1.5.5.7.3.3 -r -sv "$scriptpath\cert\root.pvk" "$scriptpath\cert\root.cer" -ss Root -sr localMachine
 		
 		# create the certificate
 		local-print  -Text "Info -- create the user certificate"
-		& $cert_prog -pe -n "CN=PowerShell User" -ss MY -a sha1 -eku 1.3.6.1.5.5.7.3.3 -iv root.pvk -ic root.cer
+		& $cert_prog -pe -n "CN=OraPowerShell SelfSignCert" -ss MY -a sha1 -eku 1.3.6.1.5.5.7.3.3 -iv "$scriptpath\cert\root.pvk" -ic "$scriptpath\cert\root.cer"
 		
 		# show the certificate
 		local-showCert
 	}
 	else {
-		local-print  -ErrorText "Error - makecert not found in $cert_prog"
+		local-print  -ErrorText "Error - makecert not found in $cert_prog_path"
 		local-print  -ErrorText ("-"*80)
 		local-print  -ErrorText "You can download makecert.exe from http://www.microsoft.com/en-us/download/details.aspx?id=8279"
 		local-print  -ErrorText ("-"*80)
@@ -145,15 +145,22 @@ function local-exportCert {
 	# define the type
 	$type = [System.Security.Cryptography.X509Certificates.X509ContentType]::pfx
 	
-	# read the cert
-	foreach ( $cert in  (dir "cert:\$certRootStore\$certStore" -codesign)) {
-		$cert
+	# search for the certificate to export
+	$use_this_certificate=local-choose-certificate -certRootStore $certRootStore -certStore $certStore
+	
+	if ($use_this_certificate) {
+		
+		$cert=$use_this_certificate
+		
 		# the the password of the certificate
 		$pass = read-host "pass" -assecurestring 
 		# export as byteset
 		$bytes = $cert.export($type, $pass)
 		# write to file
 		[System.IO.File]::WriteAllBytes($certPath, $bytes)
+	}
+	else {
+		local-print -ErrorText "Error -- certificate is null"
 	}
 }
 
@@ -188,9 +195,16 @@ function local-importCert {
 # D) show all possilbe certificates for code signing
 #
 function local-showCert{
+	param(	 [String]$certRootStore = "CurrentUser"
+			,[String]$certStore 	= "My"
+	)
 	local-print  -Text "Info -- show the existing certificates for code signing" -ForegroundColor "yellow"
 	# show the certificate
-	Get-ChildItem cert:\CurrentUser\My -codesign
+	$counter=1
+	foreach ( $cert in (Get-ChildItem "cert:\$certRootStore\$certStore" -codesign) ) {
+		local-print  -Text ( "[{0}] Subject::{1,40}  Thumbprint::{2}" -f $counter,$cert.Subject,$cert.Thumbprint )
+		$counter++
+	}
 }
 
 #==============================================================================
@@ -199,14 +213,17 @@ function local-showCert{
 function local-signScripts {
 	local-print  -Text "Info -- sign all scripts" -ForegroundColor "yellow"
 	
+	# search for the certificate to sign
+	$use_this_certificate=local-choose-certificate -certRootStore $certRootStore -certStore $certStore
+	
 	# find all ps1 and psm scripts in the script patch
 	foreach ( $script in (Get-ChildItem -Recurse -Include "*.ps1","*.psm1" -Path $scriptpath ) ) {
 		local-print  -Text "Info -- sign script $script"
-		Set-AuthenticodeSignature $script @(Get-ChildItem cert:\CurrentUser\My -codesigning)[0]
+		Set-AuthenticodeSignature $script $use_this_certificate
 		Get-AuthenticodeSignature $script
 	}
 	local-print  -Text "Info -- sign the default profile script of this session" -ForegroundColor "yellow"
-	Set-AuthenticodeSignature $profile @(Get-ChildItem cert:\CurrentUser\My -codesigning)[0]
+	Set-AuthenticodeSignature $profile $use_this_certificate
 	Get-AuthenticodeSignature $profile
 }
 
@@ -246,22 +263,85 @@ function local-resetSecSettings {
 
 function local-removeSignature {
 	local-print  -Text "Info -- remove signature from all scripts" -ForegroundColor "yellow"
-	$content=$null
+	$file_content=$null
+	
 	# find all ps1 and psm scripts in the script patch
 	foreach ( $script in (Get-ChildItem -Recurse -Include "*.ps1","*.psm1" -Path $scriptpath ) ) {
 		local-print  -Text "Info -- remove signature from script::$script"
 		#remove
-		$content=$null
-		get-Content $script | %{ if ($_ -eq '# SIG # Begin signature block') {break} else { $content+=$_;$content+=$CLF } }
-		set-Content -Path $script -value $content
+		$file_content=$null
+		$stop_process=$false
+		# break will jump out of the whole loop, not only in the pipline ....
+		get-content $script | %{ if ( $stop_process -eq $false ) { if ($_ -eq '# SIG # Begin signature block') { $stop_process=$true } else { $file_content+=$_; $file_content+=$CLF } } }
+		set-content -Path $script -value $file_content
 	}
-	local-print  -Text "Info -- remove signature from thedefault profile script of this session $profile" -ForegroundColor "yellow"
-	$content=$null
+	
+	# remove the signature from the profile of the user
+	local-print  -Text "Info -- remove signature from the default profile script of this session $profile" -ForegroundColor "yellow"
+	$file_content=$null
 	#remove
-	get-Content $profile | %{if ($_ -eq '# SIG # Begin signature block') {break} else {$content+=$_;$content+=$CLF}}
-	Set-Content -Path $profile -value $content
+	get-content $profile | %{if ($_ -eq '# SIG # Begin signature block') {break} else {$file_content+=$_; $file_content+=$CLF}}
+	Set-content -Path $profile -value $file_content
+
 }
- 
+#==============================================================================
+# show a list of certificates
+#
+function local-choose-certificate {
+ 	param(	 [String]$certRootStore = "CurrentUser"
+			,[String]$certStore 	= "My"
+	)
+	
+	local-print  -Text "Info -- List of certificates - please choose the certificat"
+	
+	# show the certificate
+	$counter=1
+	$cert_arry=@()
+	
+	foreach ( $cert in (Get-ChildItem "cert:\$certRootStore\$certStore" -codesign) ) {
+		local-print  -Text ( "[{0}] Subject::{1,40}  Thumbprint::{2}" -f $counter,$cert.Subject,$cert.Thumbprint )
+		$cert_arry+=$cert
+		$counter++
+	}
+	do {
+		# Ask questions
+		$cert_answer=Read-Host "Please enter the number of the certificate to use [1]:"
+		
+		if ($cert_answer) {
+		 # check for valid answer later
+		}
+		else {
+			$cert_answer=1
+		}
+		
+		$valid_answer="false"
+		
+		# check if the answer is in the range of the $home_count
+		try {
+			#$home_selector -match "^[\d\]+$"
+			if ($cert_answer -match "^[0123456789]+$" ) {
+			    if ($cert_answer -gt 0 ) {
+					if ($cert_answer -lt $counter ){
+						$valid_answer="true"
+					}
+				}
+			}
+		}
+		catch {
+			$valid_answer="false"
+		}
+		if ($valid_answer.equals("false")) {
+			local-print -ErrorText ("Please enter a vaild choise between 1 and {0} !" -f ($counter-1) )
+		}
+	} until ($valid_answer.equals("true"))
+	
+	$USE_THIS_CERTIFICATE=$cert_arry[$cert_answer-1]
+	
+	local-print  -Text ("You choose the certificate::{0}" -f $USE_THIS_CERTIFICATE.subject)
+	
+	return $USE_THIS_CERTIFICATE
+	
+}
 
 #==============================================================================
 # Main section of the script
@@ -276,6 +356,9 @@ function local-removeSignature {
 # E sign all ps1 scripts in the script library
 # F show the security settings
 # G set the security settings
+# I set the security settings to nothing"
+# J remove the signature"
+# X exit"
 #==============================================================================
 
 #check on Administator
@@ -306,6 +389,7 @@ if ($IsAdmin) {
 			local-print  -Text " [F] --> show the security settings"
 			local-print  -Text " [G] --> set the security settings to signed scripts"
 			local-print  -Text " [I] --> set the security settings to nothing"
+			local-print  -Text " [J] --> remove the signature"
 			local-print  -Text " [X] --> exit"
 			# Ask questions
 			$answer=Read-Host "Please enter the command you like to do:"
@@ -316,7 +400,7 @@ if ($IsAdmin) {
 		
 		# check if the answer is in the range of the List above
 		try {
-			if ($answer -imatch "^[ABCDEFGHIX]$" ) {
+			if ($answer -imatch "^[ABCDEFGHIJX]$" ) {
 			    $valid_answer="true"
 			}
 		}
@@ -356,6 +440,9 @@ if ($IsAdmin) {
 		"I" {
 			local-resetSecSettings
 		}
+		"J" {
+			local-removeSignature
+		}
 		"X" {
 			local-print  -Text "Info -- Exit"
 		}
@@ -371,27 +458,6 @@ else{
 	local-print -ForegroundColor "red"  -Text "--------------------------------------------------------------------"
 }
 #==============================================================================
-# SIG # Begin signature block
-# MIIEAwYJKoZIhvcNAQcCoIID9DCCA/ACAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
-# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
-# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUCEUDqXfQISViNCKAMFdSpHC8
-# qPGgggIdMIICGTCCAYagAwIBAgIQswsffWbgur5FbTmMnxtNqDAJBgUrDgMCHQUA
-# MBwxGjAYBgNVBAMTEUxvY2FsIENlcnRpZmljYXRlMB4XDTEyMTAwODE2NDUwMloX
-# DTM5MTIzMTIzNTk1OVowGjEYMBYGA1UEAxMPUG93ZXJTaGVsbCBVc2VyMIGfMA0G
-# CSqGSIb3DQEBAQUAA4GNADCBiQKBgQC2QvNu1p/Q30P/1MyJAadom1kOQ5r+DSk/
-# si6OX1MyZBw7KAo5VJkVFSJUWkGycVWhS5g/vi7GJUfN59nfBsa+BOL2qWbgdUXJ
-# kb4QAIFhnjLL3o0vahnFUvNLggU7e2Lb7U1HnXJ162M27piidMneo8iLULFZqbix
-# 6SNOFra8zwIDAQABo2YwZDATBgNVHSUEDDAKBggrBgEFBQcDAzBNBgNVHQEERjBE
-# gBDVerYLw85OfcC2L05Gi3JRoR4wHDEaMBgGA1UEAxMRTG9jYWwgQ2VydGlmaWNh
-# dGWCEN6vlJpqhmiXQJ+5DWLZODswCQYFKw4DAh0FAAOBgQAtqLl71Y/h9pdorhGx
-# TaQ+wHgjkBJ7YrVxYphnzdO0rQU8hPVSQr0cH3YX0IpAc7IRsbf3GPPldnAAk+Cs
-# qOZ2PZtLDULf/wNhQ37rjfmBNZoHFRzSdaaEb1goFiTjHs4M1JmS3ZA0Uo89lSnA
-# fr8wpIA2Al0lRo3Cys1EH4p/qzGCAVAwggFMAgEBMDAwHDEaMBgGA1UEAxMRTG9j
-# YWwgQ2VydGlmaWNhdGUCELMLH31m4Lq+RW05jJ8bTagwCQYFKw4DAhoFAKB4MBgG
-# CisGAQQBgjcCAQwxCjAIoAKAAKECgAAwGQYJKoZIhvcNAQkDMQwGCisGAQQBgjcC
-# AQQwHAYKKwYBBAGCNwIBCzEOMAwGCisGAQQBgjcCARUwIwYJKoZIhvcNAQkEMRYE
-# FMVFq9Mj1CAl4shrTZhdIb9RnAcfMA0GCSqGSIb3DQEBAQUABIGAGHTuNGxoosG6
-# YGF68EoAFZ/KuayIQ3G0kQNrXJLy8dqABpUVz5/d9aIHLNNiMvwd3k2dv2U9WSQp
-# vBOmGsNVjqFbJrzjIrNUd2koSq0ZKTc76JNQ6x9lt3F03SDWZW9CUPWxjk+srlPo
-# Wymv+fF4mSku2O8p3oXD7fKh3Plmob0=
-# SIG # End signature block
+
+
+
