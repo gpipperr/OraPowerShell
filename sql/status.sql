e33b03e2b1b2f0f04e61aea2dec7f936ef14f354
gpipperr <gpipperr@localhost>
2015-10-28 17:54:41 +0100

    Add quiesce status to status.sql new Script to call awr Reports from sql*Plus

--- sql/status.sql
+++ sql/status.sql
@@ -13,9 +13,13 @@ column status    format A8  heading "Status"
 column name      format A8  heading "Instance|Name"
 column startzeit format A15 heading "Start|Time"
 column host_name format A25 heading "Server|Name"
+column logins    format a10 heading "Check|Restrict"
+column active_state format a10 heading "Check|Quiesce "
 
 select inst_id
       , status
+	  , logins
+	  , active_state
       , instance_name as name
       , to_char(STARTUP_TIME, 'dd.mm.YY hh24:mi') as startzeit
 	  , host_name 
