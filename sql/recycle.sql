986c892eb5912231a7361b3147ccd38476a1dfe6
gpipperr <gpipperr@localhost>
2015-12-28 12:55:05 +0100

    Fix block size problem

--- sql/recycle.sql
+++ sql/recycle.sql
@@ -5,22 +5,38 @@
 --==============================================================================
 set linesize 130 pagesize 300 recsep off
 
+set verify off
+
 column owner format a15 heading "Owner"
 column TYPE  format a12 heading "Obj|type"
 
 column max_CREATETIME format a14 heading "Min|Create"
 column min_CREATETIME format a14 heading "Max|Create"
 column min_DROPTIME   format a14 heading "Min Age|Drop"
-column max_DROPTIME   format a14 heading "Max Age|Drop"
-column SPACE_GB       format 999G999D99 heading "Space|GB"
+column max_DROPTIME   format a16 heading "Max Age|Drop"
+column SPACE_GB       format 999G999D999 heading "Space|GB"
 
 --
---fix use DB Block size!!
+--get first the blocksize
 --
 --define BLOCK_SIZE=8192
-define BLOCK_SIZE=16384
+--define BLOCK_SIZE=16384
+set termout off
 
 
+col BLOCK_SIZE_COL new_val BLOCK_SIZE
+ 
+SELECT value as BLOCK_SIZE_COL
+  FROM v$parameter
+ WHERE name = 'db_block_size';
+/
+
+set termout  on
+
+--
+show parameter recyclebin
+
+---
 column DUMMY NOPRINT;
 COMPUTE SUM OF SPACE_GB ON DUMMY;
 BREAK ON DUMMY;
@@ -33,8 +49,8 @@ select null dummy
 	  , substr(min(CREATETIME),1,13)  as min_CREATETIME
 	  , substr(max(CREATETIME),1,13)  as max_CREATETIME
 	  , substr(min(DROPTIME),1,13)    as min_DROPTIME
-	  , substr(max(DROPTIME),1,13)    as max_DROPTIME
-	  , round(((sum(space)*&&BLOCK_SIZE)/1024/1024/1024),2) as SPACE_GB
+	  , substr(max(DROPTIME),1,16)    as max_DROPTIME
+	  , round(((sum(space)*&&BLOCK_SIZE)/1024/1024/1024),3) as SPACE_GB
  from DBA_RECYCLEBIN
 group by owner
        , type
@@ -45,3 +61,5 @@ prompt .....
 prompt ..... -- to clean all:  PURGE DBA_RECYCLEBIN ( as sys user!)
 prompt ..... -- to clean your bin : PURGE RECYCLEBIN; 
 prompt .....
+
+set verify on
