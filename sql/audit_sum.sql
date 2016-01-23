11bca468228a33062680fd6150ccce71acecf838
Gunther Pippèrr <18365641+gpipperr@users.noreply.github.com>
2016-01-23 16:45:53 +0100

    Fix Typos Add script to get all possible user env settings FTP / http ports of the database

--- sql/audit_sum.sql
+++ sql/audit_sum.sql
@@ -43,6 +43,9 @@ column os_username  format a16 heading "User|Name"
 column username     format a15  heading "DB User|name"
 column action_name format a20  heading "Action|name"
 column instance_number format 99 heading "In|st"
+column user_host  format a11 heading "User|Host"
+column first_log   format a15  heading "First|entry"
+column last_log    format a15  heading "Last|entry"
 
 break on instance_number
 --COMPUTE SUM OF action_count ON first_log
@@ -52,6 +55,7 @@ ttitle left  "Audit log summary overview " skip 2
 
 select    count(*) as action_count
      ,  os_username
+	 ,  (case  when length(USERHOST) > 10  then '..'||substr(USERHOST,-8,10) else USERHOST end)  as user_host
      ,  username
 	 ,  instance_number
 	 ,  to_char(min(extended_timestamp),'dd.mm hh24:mi') as first_log
@@ -60,7 +64,8 @@ select    count(*) as action_count
   from dba_audit_trail 
  where  extended_timestamp between sysdate -(1/4) and sysdate
        -- extended_timestamp between to_date('13.11.2014 12:19','dd.mm.yyyy hh24:mi') and to_date('13.11.2014 12:21','dd.mm.yyyy hh24:mi')		 
- group by  os_username
+ group by  os_username 
+     ,  (case  when length(USERHOST) > 10  then '..'||substr(USERHOST,-8,10) else USERHOST end) 
      ,  username
     -- ,  to_char(extended_timestamp,'dd.mm hh24')
 	 ,  action_name 		 
