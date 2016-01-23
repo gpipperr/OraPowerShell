a805807697cd00805e21b283106546976616edf6
gpipperr <gpipperr@localhost>
2016-01-23 17:00:16 +0100

    Fix Typos

--- ps/create_ctx_report.sql
+++
@@ -1,4 +0,0 @@
-select ctx_report.describe_index('CTXSYS.STOP_OPT_LIST') from dual;                                                     
-select ctx_report.describe_index('CTXSYS.ENT_EXT_DICT_OBJ') from dual;                                                  
-select ctx_report.describe_index('CTXSYS.DEFAULT_POLICY_ORACONTAINS') from dual;                                        
-select ctx_report.describe_index('CTXSYS.AUTO_OPT_OBJ') from dual;                                                      
